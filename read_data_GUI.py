0cc54d4e45612c1d83358c95307451fc9c56987e
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2019-04-25 03:30:26 +0200

    Update read_data_GUI.py

--- read_data_GUI.py
+++ read_data_GUI.py
@@ -1,7 +1,60 @@
 import sys
+import json
+import matplotlib.pyplot as plt
+from PyQt5.QtGui import *
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUi
+
+
+class MonteCarloItem(QWidget):
+    def __init__ (self, agent):
+        super(MonteCarloItem, self).__init__()
+        loadUi("GUI/MonteCarloItem.ui", self)
+
+        self.type.setText("MonteCarlo")
+        self.epsilon.setText(agent["epsilon"])
+        self.gamma.setText(agent["gamma"])
+        self.n_games.setText(agent["n_games"])
+        self.n_episodes.setText(agent["n_episodes"])
+
+class DynamicProgrammingItem(QWidget):
+    def __init__ (self, agent):
+        super(DynamicProgrammingItem, self).__init__()
+        loadUi("GUI/DynamicProgrammingItem.ui", self)
+
+        self.type.setText("Dynamic programming")
+        self.gamma.setText(agent["gamma"])
+        self.theta.setText(agent["theta"])
+
+class QLearningItem(QWidget):
+    def __init__ (self, agent):
+        super(QLearningItem, self).__init__()
+        loadUi("GUI/QLearningItem.ui", self)
+
+        self.type.setText("Q-Learning")
+        self.alpha.setText(agent["alpha"])
+        self.gamma.setText(agent["gamma"])
+        self.epsilon.setText(agent["epsilon"])
+        self.n_games.setText(agent["n_games"])
+        self.n_episodes.setText(agent["n_episodes"])
+
+class NStepSarsaItem(QWidget):
+    def __init__ (self, agent):
+        super(NStepSarsaItem, self).__init__()
+        loadUi("GUI/NStepSarsaItem.ui", self)
+
+        self.type.setText("n-step SARSA")
+        self.n_step.setText(agent["n_step"])
+        self.alpha.setText(agent["alpha"])
+        self.gamma.setText(agent["gamma"])
+        self.epsilon.setText(agent["epsilon"])
+        self.n_games.setText(agent["n_games"])
+        self.n_episodes.setText(agent["n_episodes"])
+
+
+
 
 
 
@@ -10,9 +63,33 @@ class AppWindow(QDialog):
         super(AppWindow, self).__init__()
         loadUi("GUI/gui.ui", self)
         self.setWindowTitle("Read data GUI")
-        self.add_mc_to_graph.clicked.connect(self.add_mc_to_graph_clicked)
 
+        self.add_enviroment.clicked.connect(self.add_enviroment_clicked)
+        self.add_tests_moment.clicked.connect(self.add_tests_moment_clicked)
+
+        self.add_mc_to_graph.clicked.connect(self.add_mc_to_graph_clicked)
+        self.add_dp_to_graph.clicked.connect(self.add_dp_to_graph_clicked)
+        self.add_ql_to_graph.clicked.connect(self.add_ql_to_graph_clicked)
+        self.add_nss_to_graph.clicked.connect(self.add_nss_to_graph_clicked)
+
+
+
+        self.delete_current_item.clicked.connect(self.delete_current_item_clicked)
         self.show_graph.clicked.connect(self.show_graph_clicked)
+
+
+    @pyqtSlot()
+    def add_enviroment_clicked(self):
+        del agent_list[:]
+        self.agent_list_recap.clear()
+        self.enviroment_name_recap.setText(self.enviroment_name.text())
+
+    @pyqtSlot()
+    def add_tests_moment_clicked(self):
+        del agent_list[:]
+        self.agent_list_recap.clear()
+        self.tests_moment_recap.setText(self.tests_moment.text())
+
 
 
 
@@ -26,25 +103,147 @@ class AppWindow(QDialog):
             "epsilon": self.mc_epsilon.text(),
             "gamma": self.mc_gamma.text()
         }
+        agent_list.append(agent)
+        Item = QtWidgets.QListWidgetItem(self.agent_list_recap)
+        Item_Widget = MonteCarloItem(agent)
+        Item.setSizeHint(Item_Widget.sizeHint())
+        self.agent_list_recap.addItem(Item)
+        self.agent_list_recap.setItemWidget(Item, Item_Widget)
 
+    @pyqtSlot()
+    def add_dp_to_graph_clicked(self):
+
+        agent ={
+            "type": "Dynamic programming",
+            "gamma": self.dp_gamma.text(),
+            "theta": self.dp_theta.text()
+        }
+        agent_list.append(agent)
+        Item = QtWidgets.QListWidgetItem(self.agent_list_recap)
+        Item_Widget = DynamicProgrammingItem(agent)
+        Item.setSizeHint(Item_Widget.sizeHint())
+        self.agent_list_recap.addItem(Item)
+        self.agent_list_recap.setItemWidget(Item, Item_Widget)
+
+    @pyqtSlot()
+    def add_ql_to_graph_clicked(self):
+
+        agent ={
+            "type": "Q learning",
+            "alpha": self.ql_alpha.text(),
+            "gamma": self.ql_gamma.text(),
+            "epsilon": self.ql_epsilon.text(),
+            "n_games": self.ql_n_games.text(),
+            "n_episodes": self.ql_n_episodes.text()
+        }
+        agent_list.append(agent)
+        Item = QtWidgets.QListWidgetItem(self.agent_list_recap)
+        Item_Widget = QLearningItem(agent)
+        Item.setSizeHint(Item_Widget.sizeHint())
+        self.agent_list_recap.addItem(Item)
+        self.agent_list_recap.setItemWidget(Item, Item_Widget)
+
+    @pyqtSlot()
+    def add_nss_to_graph_clicked(self):
+
+        agent ={
+            "type": "n-step SARSA",
+            "alpha": self.nss_alpha.text(),
+            "gamma": self.nss_gamma.text(),
+            "epsilon": self.nss_epsilon.text(),
+            "n_games": self.nss_n_games.text(),
+            "n_episodes": self.nss_n_episodes.text(),
+            "n_step": self.nss_n_step.text()
+        }
+        agent_list.append(agent)
+        Item = QtWidgets.QListWidgetItem(self.agent_list_recap)
+        Item_Widget = NStepSarsaItem(agent)
+        Item.setSizeHint(Item_Widget.sizeHint())
+        self.agent_list_recap.addItem(Item)
+        self.agent_list_recap.setItemWidget(Item, Item_Widget)
+
+
+    @pyqtSlot()
+    def delete_current_item_clicked(self):
+
+        for selectedItem in self.agent_list_recap.selectedItems():
+            del agent_list[self.agent_list_recap.row(selectedItem)]
+            self.agent_list_recap.takeItem(self.agent_list_recap.row(selectedItem))
 
 
 
     @pyqtSlot()
     def show_graph_clicked(self):
 
-        enviroment_name = self.enviroment_name.text()
-        tests_moment = self.tests_moment.text()
+        path = "docs/" + self.enviroment_name_recap.text() + "/" + self.tests_moment_recap.text() + "/" + agent_list[0]["type"] + "/" + create_agent_params_string(agent_list[0])
 
-        path = "docs/" + enviroment_name + "/" + tests_moment + "/"
-        #path = "docs/" + enviroment_name + "/" + tests_moment + "/" + agent["type"] + "/" + create_agent_params_string(agent)
+        with open(path) as inputfile:
+            tests_list = json.load(inputfile)
 
-        self.enviroment_name.setText(path)
+        legend = []
+
+        for test_type in tests_list[0]:
+            plt.figure(test_type)
+
+            for test_agent in range(len(tests_list)):
+
+                legend.append(create_legend_string(agent_list[0]))
+                plt.plot(tests_list[test_agent][test_type])
+
+            plt.legend(legend, loc='upper left')
+            plt.ylabel(test_type)
+
+        plt.show()
+
+
+
+
+
+
+def create_agent_params_string(agent):
+
+    string = ""
+
+    if agent["type"] == "MonteCarlo" or agent["type"] == "MC":
+        return "Epsilon= " + str(agent["epsilon"]) + ", gamma= " + str(agent["gamma"]) + ", n_games= " + str(agent["n_games"]) + ", n_episodes= " + str(agent["n_episodes"])
+
+    elif agent["type"] == "Dynamic programming" or agent["type"] == "DP":
+        return "Gamma= " + str(agent["gamma"]) + ", theta= " + str(agent["theta"])
+
+    elif agent["type"] == "Q learning" or agent["type"] == "QL":
+        return "Alpha= " + str(agent["alpha"]) + ", gamma= " + str(agent["gamma"]) + ", epsilon= " + str(agent["epsilon"]) + ", n_games= " + str(agent["n_games"]) + ", n_episodes= " + str(agent["n_episodes"])
+
+    elif agent["type"] == "n-step SARSA" or agent["type"] == "NSS":
+        return "N-step= " + str(agent["n_step"]) + ", alpha= " + str(agent["alpha"]) + ", gamma= " + str(agent["gamma"]) + ", epsilon= " + str(agent["epsilon"]) + ", n_games= " + str(agent["n_games"]) + ", n_episodes= " + str(agent["n_episodes"])
+
+
+def create_legend_string(agent):
+
+    string = ""
+
+    if agent["type"] == "MonteCarlo" or agent["type"] == "MC":
+        return "MonteCarlo, epsilon=" + str(agent["epsilon"]) + ", gamma=" + str(agent["gamma"]) + ", n_games=" + str(agent["n_games"]) + ", n_episodes=" + str(agent["n_episodes"])
+
+    elif agent["type"] == "Dynamic programming" or agent["type"] == "DP":
+        return "Dynamic programming, gamma=" + str(agent["gamma"]) + ", theta=" + str(agent["theta"])
+
+    elif agent["type"] == "Q learning" or agent["type"] == "QL":
+        return "Q learning, alpha=" + str(agent["alpha"]) + ", gamma=" + str(agent["gamma"]) + ", epsilon=" + str(agent["epsilon"]) + ", n_games=" + str(agent["n_games"]) + ", n_episodes=" + str(agent["n_episodes"])
+
+    elif agent["type"] == "n-step SARSA" or agent["type"] == "NSS":
+        return "n-step SARSA, n-step=" + str(agent["n_step"]) + ",alpha=" + str(agent["alpha"]) + ", gamma=" + str(agent["gamma"]) + ", epsilon=" + str(agent["epsilon"]) + ", n_games=" + str(agent["n_games"]) + ", n_episodes=" + str(agent["n_episodes"])
 
 
 
 
 if __name__ == '__main__':
+
+    agent_list = []
+
+
+
+    enviroment_name = ""
+    tests_moment = ""
 
 
     app = QApplication(sys.argv)
@@ -52,26 +251,3 @@ if __name__ == '__main__':
     w.show()
 
     sys.exit(app.exec_())
-
-
-
-
-
-    with open(path) as inputfile:
-        tests_list = json.load(inputfile)
-
-
-    legend = []
-
-    for test_type in tests_list[0]:
-        plt.figure(test_type)
-
-        for test_agent in range(len(tests_list)):
-
-            legend.append(create_legend_string(agent))
-            plt.plot(tests_list[test_agent][test_type])
-
-        plt.legend(legend, loc='upper left')
-        plt.ylabel(test_type)
-
-    plt.show()
